0bb2a695ee449e58e72f52b976337419fb87d69a
arpansharma <arpan.sharma011@gmail.com>
2021-01-02 13:46:03 +0530

    Added basic admin for accounts app

--- whats_my_share/accounts/admin.py
+++ whats_my_share/accounts/admin.py
@@ -1,3 +1,26 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+# app level imports
+from .models import User, Group
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'username',
+        'first_name',
+        'last_name',
+        'email',
+    )
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        # 'username',
+        # 'first_name',
+        # 'last_name',
+        # 'email',
+    )
