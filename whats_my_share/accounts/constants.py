0bfdc54b2ca3fb6498fd1c102402f63fcdc30e8a
arpansharma <arpan.sharma011@gmail.com>
2021-01-02 15:59:50 +0530

    Added error codes for returning with requests with status 400

---
+++ whats_my_share/accounts/constants.py
@@ -0,0 +1,19 @@
+INCORRECT_PASSWORD = {
+    'code': 'USR_400_1',
+    'detail': 'Provided password is incorrect.',
+}
+
+INCORRECT_USERNAME = {
+    'code': 'USR_400_2',
+    'detail': 'Provided username(s) are incorrect.',
+}
+
+GROUP_ALREADY_EXISTS = {
+    'code': 'USR_400_3',
+    'detail': 'Provided group already exists.',
+}
+
+GROUP_DOES_NOT_EXISTS = {
+    'code': 'USR_400_4',
+    'detail': 'Provided group does not exists.',
+}
