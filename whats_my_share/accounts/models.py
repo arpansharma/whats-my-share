a57c4265d184b2922710780db735c52f7507e220
arpansharma <arpan.sharma011@gmail.com>
2021-01-02 12:11:33 +0530

    Adding Group model to accounts app

--- whats_my_share/accounts/models.py
+++ whats_my_share/accounts/models.py
@@ -16,3 +16,22 @@ class User(AbstractUser):
         app_label = 'accounts'
         db_table = 'api_user'
         verbose_name_plural = 'Users'
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=64)
+    owner = models.ForeignKey('accounts.user', related_name="owner_user", on_delete=models.PROTECT)
+    members = models.ManyToManyField('accounts.User', related_name='members_users')
+    profile_picure = models.URLField(max_length=512, null=True, default=None)
+
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        app_label = 'accounts'
+        db_table = 'api_group'
+        verbose_name_plural = 'Groups'
