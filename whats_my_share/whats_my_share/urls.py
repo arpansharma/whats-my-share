fce543db04511b3840004422142bc6e05f9dd5aa
arpansharma <arpan.sharma011@gmail.com>
2021-01-02 20:15:13 +0530

    Added API endpoint for expense app

--- whats_my_share/whats_my_share/urls.py
+++ whats_my_share/whats_my_share/urls.py
@@ -5,10 +5,14 @@ from rest_framework.routers import SimpleRouter
 
 # project level imports
 from accounts import views as user_views
+from expense import views as expense_views
 
 router = SimpleRouter()
+
 router.register(r'accounts/user', user_views.UserViewSet, basename='accounts')
 router.register(r'accounts/group', user_views.GroupViewSet, basename='accounts')
+
+router.register(r'expense', expense_views.ExpenseViewSet, basename='expense')
 
 urlpatterns = [
     path('api/v1/', include(router.urls)),
