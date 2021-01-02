a78b3a98a60f6111f70cfc2ea2a406a121db46bc
arpansharma <arpan.sharma011@gmail.com>
2021-01-02 16:05:29 +0530

    Added APIs related to Group

--- whats_my_share/accounts/serializers.py
+++ whats_my_share/accounts/serializers.py
@@ -21,3 +21,25 @@ class AuthenticateSerializer(serializers.Serializer):
     """
     username = serializers.CharField(max_length=150, allow_null=False, allow_blank=False)
     password = serializers.CharField(max_length=128, allow_null=False, allow_blank=False)
+
+
+class CreateSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=64, allow_null=False, allow_blank=False)
+
+
+class AddMemberSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=64, allow_null=False, allow_blank=False)
+    members = serializers.ListField(
+        child=serializers.CharField(max_length=150, allow_null=False, allow_blank=False),
+        allow_empty=False,
+        min_length=1,
+    )
+
+
+class RemoveMemberSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=64, allow_null=False, allow_blank=False)
+    members = serializers.ListField(
+        child=serializers.CharField(max_length=150, allow_null=False, allow_blank=False),
+        allow_empty=False,
+        min_length=1,
+    )
--- whats_my_share/accounts/services.py
+++ whats_my_share/accounts/services.py
@@ -1,8 +1,15 @@
 # django / rest-framework imports
+from rest_framework.exceptions import ParseError
 from rest_framework.authtoken.models import Token
 
 # app level imports
-from .models import User
+from .models import User, Group
+from .constants import (
+    INCORRECT_PASSWORD,
+    INCORRECT_USERNAME,
+    GROUP_ALREADY_EXISTS,
+    GROUP_DOES_NOT_EXISTS,
+)
 
 
 class UserService:
@@ -15,6 +22,9 @@ class UserService:
             if user.check_password(password):
                 auth_token = Token.objects.filter(user__username=username)
                 return auth_token.last().key
+
+            raise ParseError(INCORRECT_PASSWORD)
+        raise ParseError(INCORRECT_USERNAME)
 
     def register_user(validated_data):
         user = User.objects.create_user(
@@ -35,3 +45,44 @@ class UserService:
 
         auth_token = UserService.retrieve_token(username=username, password=password)
         return auth_token
+
+
+class GroupService:
+    def create_group(validated_data):
+        name = validated_data['name']
+        owner = validated_data['owner']
+
+        if Group.objects.filter(name=name).exists():
+            raise ParseError(GROUP_ALREADY_EXISTS)
+
+        Group.objects.create(name=name, owner=owner)
+
+    def add_members(validated_data):
+        name = validated_data['name']
+        owner = validated_data['owner']
+        member_usernames = validated_data['members']
+
+        if not Group.objects.filter(name=name).exists():
+            raise ParseError(GROUP_DOES_NOT_EXISTS)
+
+        new_members = User.objects.filter(username__in=member_usernames)
+        if new_members.count() != len(member_usernames):
+            raise ParseError(INCORRECT_USERNAME)
+
+        group = Group.objects.get(name=name, owner=owner)
+        group.members.add(*new_members)
+
+    def remove_members(validated_data):
+        name = validated_data['name']
+        owner = validated_data['owner']
+        member_usernames = validated_data['members']
+
+        if not Group.objects.filter(name=name).exists():
+            raise ParseError(GROUP_DOES_NOT_EXISTS)
+
+        remove_members = User.objects.filter(username__in=member_usernames)
+        if remove_members.count() != len(member_usernames):
+            raise ParseError(INCORRECT_USERNAME)
+
+        group = Group.objects.get(name=name, owner=owner)
+        group.members.remove(*remove_members)
