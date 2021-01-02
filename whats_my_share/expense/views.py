ca3b53cdb3c786850c394104c64ed3d3bb7a76c2
arpansharma <arpan.sharma011@gmail.com>
2021-01-02 20:16:20 +0530

    Added API endpoint to create a new expense

--- whats_my_share/expense/views.py
+++ whats_my_share/expense/views.py
@@ -1,3 +1,31 @@
-# from django.shortcuts import render
+# django/rest-framework imports
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.exceptions import ParseError
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+# app level imports
+from .serializers import CreateSerializer
+
+
+class ExpenseViewSet(GenericViewSet):
+    http_method_names = ['post']
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    serializers = {
+        "create": CreateSerializer,
+    }
+
+    def get_serializer_class(self):
+        return self.serializers[self.action]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid() is False:
+            raise ParseError(serializer.errors)
+
+        return Response({
+            'message': 'Expense/Bill has been added successfully.',
+        })
