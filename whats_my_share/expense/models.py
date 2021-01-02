1b0e5ea808bd0673c7927d68cf5f25eb193220bd
arpansharma <arpan.sharma011@gmail.com>
2021-01-02 17:54:15 +0530

    :fix: Addd unique related_name identifiers in Ledger Timeline

--- whats_my_share/expense/models.py
+++ whats_my_share/expense/models.py
@@ -32,18 +32,18 @@ class Expense(models.Model):
         verbose_name_plural = 'Expenses'
 
 
-class LedgerTimeline:
+class LedgerTimeline(models.Model):
     EVENT_CHOICES = (
         ('expense', 'Shared an Expense'),
         ('settlement', 'Did a Settlement'),
     )
 
     event = models.CharField(max_length=16, choices=EVENT_CHOICES)
-    credit_to = models.ForeignKey('accounts.user', on_delete=models.PROTECT)
-    debit_from = models.ForeignKey('accounts.user', on_delete=models.PROTECT)
+    credit_to = models.ForeignKey('accounts.user', related_name="credit_to_lt", on_delete=models.PROTECT)
+    debit_from = models.ForeignKey('accounts.user', related_name="debit_from_lt", on_delete=models.PROTECT)
     amount = models.DecimalField(max_digits=11, decimal_places=2)
-    expense = models.ForeignKey('expense.Expense', on_delete=models.PROTECT)
-    created_by = models.ForeignKey('accounts.User', on_delete=models.PROTECT)
+    expense = models.ForeignKey('expense.Expense', related_name="expense_lt", on_delete=models.PROTECT)
+    created_by = models.ForeignKey('accounts.User', related_name="created_by_lt", on_delete=models.PROTECT)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
